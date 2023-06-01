2298ad73b40711dd3cf2a45bf32cbeb66eeff994
thebroxaz <126773223+thebroxaz@users.noreply.github.com>
2023-05-31 22:59:17 -0300

    Update Fixo.java

--- 2º Bimestre/Herança/src/Fixo.java
+++ 2º Bimestre/Herança/src/Fixo.java
@@ -1,11 +1,5 @@
 public class Fixo extends Gastos{
-
     public Fixo(String nome, double valor) {
         super(nome, valor);
-    }
-
-    @Override
-    public void MudarValor(double valor) {
-
     }
 }
