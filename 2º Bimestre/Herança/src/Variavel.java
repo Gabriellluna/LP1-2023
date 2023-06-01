7d328da4b6449ddb5268c5f367fb7c035a9756d5
thebroxaz <126773223+thebroxaz@users.noreply.github.com>
2023-05-31 23:02:12 -0300

    Update Variavel.java

--- 2º Bimestre/Herança/src/Variavel.java
+++ 2º Bimestre/Herança/src/Variavel.java
@@ -1,8 +1,24 @@
 public class Variavel extends Gastos{
+    private int tempoMeses;
 
-    public Variavel(String nome, double valor) {
+    //construtor
+    public Variavel(String nome, double valor, int tempoMeses) {
         super(nome, valor);
+        this.tempoMeses=tempoMeses;
     }
 
-    private int TempoMeses;
+    //getter
+    public int getTempoMeses() {
+        return tempoMeses;
+    }
+
+    //setter
+    public void setTempoMeses(int tempoMeses) {
+        this.tempoMeses = tempoMeses;
+    }
+
+    public double Valor_Total(double valorT){
+        valorT = valor * tempoMeses;
+        return valorT;
+    }
 }
